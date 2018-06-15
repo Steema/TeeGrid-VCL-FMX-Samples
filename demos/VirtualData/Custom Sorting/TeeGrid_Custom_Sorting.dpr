e8e942f6b204e79e7247f0849564abf66b1a87f0
Yeray Alonso <yeray@steema.com>
2018-06-15 09:43:43 +0200

    Add reference to Unit_MyData in VirtualData/Custom_Sorting demo

--- demos/VirtualData/Custom Sorting/TeeGrid_Custom_Sorting.dpr
+++ demos/VirtualData/Custom Sorting/TeeGrid_Custom_Sorting.dpr
@@ -2,7 +2,8 @@ program TeeGrid_Custom_Sorting;
 
 uses
   Vcl.Forms,
-  Unit_Custom_Sorting in 'Unit_Custom_Sorting.pas' {Form1};
+  Unit_Custom_Sorting in 'Unit_Custom_Sorting.pas' {Form1},
+  Unit_MyData in '..\Unit_MyData.pas';
 
 {$R *.res}
 
